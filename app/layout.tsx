823d936546fceff119b1c1a78b7f391528b0d5eb
ChoChungEun <82curst@gmail.com>
2024-01-21 15:08:07 +0900

    add matine global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/Nav";
+import "@mantine/core/styles.css";
+import { Nav } from "@/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
