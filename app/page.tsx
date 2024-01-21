1591d2bca42a7907c95893423553f1bca961e8b6
ChoChungEun <82curst@gmail.com>
2024-01-21 15:08:22 +0900

    add provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+import { Nav } from "@/components/Nav";
+import Timer from "@/components/Timer";
+import { MantineProvider } from "@mantine/core";
+import { RecoilRoot } from "recoil";
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <RecoilRoot>
+      <MantineProvider>
+        <Timer />
+      </MantineProvider>
+    </RecoilRoot>
+  );
 }
